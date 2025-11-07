52ee526ddc80d93080a19066487177bdf645230a
Reymon Mercado <rmercado@gvrealtors.ca>
2025-11-07 11:18:10 -0800

    added displaying of images

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,7 +1,9 @@
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
-import { notFound } from "next/navigation";
-import Link from "next/link";
+import { urlFor } from "@/sanity/lib/image";
 
 export default async function Page({
   params,
@@ -18,7 +20,21 @@ export default async function Page({
   }
 
   return (
-    <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
+    <main className="container mx-auto grid grid-cols-1 gap-6 py-12">
+      {post?.mainImage ? (
+        <Image
+          className="w-full aspect-800/300"
+          src={urlFor(post.mainImage)
+            .width(800)
+            .height(300)
+            .quality(80)
+            .auto("format")
+            .url()}
+          alt={post?.mainImage?.alt || ""}
+          width="800"
+          height="300"
+        />
+      ) : null}
       <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
       <hr />
       <Link href="/posts">&larr; Return to index</Link>
